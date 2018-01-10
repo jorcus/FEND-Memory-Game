7be0c84dc78956225a87f3052da5dc010e7328d1
NG FANG KIANG <kiang.ng@hotmail.com>
2018-01-10 12:51:05 +0800

    timer bug fixed

--- js/app.js
+++ js/app.js
@@ -20,14 +20,8 @@ var list = [
     "fa-cube"
 ];
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+// Shuffle the list of cards function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     "use strict";
     var currentIndex = array.length,
@@ -43,7 +37,7 @@ function shuffle(array) {
     return array;
 }
 
-// Append the <li> after shuffle the card.
+// Loop through each card and create its HTML <li> after shuffle the card.
 function add_list(list) {
     "use strict";
     $("ul#cards").children().remove("li"); // FOR RESET THE GAME
@@ -53,57 +47,20 @@ function add_list(list) {
 
 }
 
-function click_card() {
+
+function score_ratings(moves) {
     "use strict";
+    var all_stars = $(".stars");
+    if ((moves > 12) && (moves <= 15)) {
+        all_stars.children().remove("li");
+        all_stars.append("<li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li><li><i class='fa fa-star-o'></i></li>"); // Two stars
 
-    var previous_card_selector = "";
-    var previous_card_icon = "";
-    var move_count = 0;
-    var card_selected = false;
-	
-	$(".moves").text(move_count); // FOR RESET THE GAME
-	
-	
-    $(".card").on("click", function() {
-        var current_card = $(this).attr("class");
-        var current_card_icon = $(this).children().attr("class");
+    } else if (moves > 17) {
+        all_stars.children().remove("li");
+        all_stars.append("<li><i class='fa fa-star'></i></li><li><i class='fa fa-star-o'></i></li><li><i class='fa fa-star-o'></i></li>"); // A star
+    }
 
-		
-		// Card matching logics and set up the event listener for a card. If a card is clicked
-        if (current_card === "card") {
-            if ((current_card_icon === previous_card_icon) && (card_selected === true)) {  //if both cards matched, lock the cards in the open position 
-                $(this).addClass("match");
-                previous_card_selector.removeClass("open show").addClass("match");
-                card_selected = false;
-				move_count ++;
 
-            } else if ((card_selected === true)) {
-				// if the cards do not match, lock the cards in the open position and disable opening others card. Then, remove the cards from the list, hide the card's symbol and reactive ability to click others card.
-                $(this).addClass("open show");
-				$(".card").addClass("disabled");
-                setTimeout(function() {
-                    $(".open.show").removeClass("open show");
-					$(".card").removeClass("disabled");
-                }, 800);
-                card_selected = false;
-				move_count ++;
-
-            } else {
-				// Default click and show the card
-				
-                $(this).addClass("open show");
-                previous_card_icon = current_card_icon;
-                previous_card_selector = $(this);
-                card_selected = true;
-				
-            }
-
-			$(".moves").text(move_count);
-        }
-		
-		
-		
-    });
 }
 
 
@@ -111,15 +68,90 @@ function startGame() {
     "use strict";
     list = shuffle(list);
     add_list(list);
-    click_card();
+
+
+    var previous_card_selector = "";
+    var previous_card_icon = "";
+    var move_count = 0;
+    var card_selected = false;
+    var timer = 0;
+    var the_timer;
+
+
+    $(".card").on("click", function() {
+		timer++;  // This will activate the timer/stopwatch
+        var current_card = $(this).attr("class");
+        var current_card_icon = $(this).children().attr("class");
+
+
+        // Card matching logics and set up the event listener for a card. If a card is clicked
+        if (current_card === "card") {
+
+            if ((current_card_icon === previous_card_icon) && (card_selected === true)) {
+                //if both cards matched, lock the cards in the open position 
+                $(this).addClass("match");
+                previous_card_selector.removeClass("open show").addClass("match");
+                card_selected = false;
+                move_count++;
+
+            } else if ((card_selected === true)) {
+                // if the cards do not match, lock the cards in the open position and disable opening others card. Then, remove the cards from the list, hide the card's symbol and reactive ability to click others card.
+                $(this).addClass("open show");
+                $(".card").addClass("disabled");
+                setTimeout(function() {
+                    $(".open.show").removeClass("open show");
+                    $(".card").removeClass("disabled");
+                }, 800);
+                card_selected = false;
+                move_count++;
+
+            } else {
+                // Default click and show the card
+                $(this).addClass("open show");
+                previous_card_icon = current_card_icon;
+                previous_card_selector = $(this);
+                card_selected = true;
+            }
+
+            // Updating move count to web pages
+            $(".moves").text(move_count);
+
+            // Ratings from 3 stars to 1 stars
+            score_ratings(move_count);
+
+
+			// TIMER function
+			if (timer === 1) {
+                var startTime = Date.now();
+                the_timer = setInterval(function() {
+                    var elapsedTime = Date.now() - startTime;
+                    $("#timer").text((elapsedTime / 1000).toFixed(3));
+                }, 44);
+
+            }  
+
+			
+			// RESET FUNCTION 
+            $(".fa-repeat").click(function() {
+				    $(".stars").children().remove("li");
+    $(".stars").append("<li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li>"); // Full stars
+    $(".moves").text(move_count); // FOR RESET THE GAME
+					clearInterval(the_timer);
+	$("#timer").text("0.000");
+                startGame();
+            });
+        }
+
+    });
+
+
 }
 
 
+
+
 startGame();
-$(".fa-repeat").click(function() {
-    "use strict";
-    startGame();
-});
+
 
 
 
